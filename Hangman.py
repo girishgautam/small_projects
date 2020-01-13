2ffc1bba415173521851d80092d4f96f9a52bdf7
Girish Joshi <girishgautam@gmail.com>
2020-01-13 22:16:55 +0000

    added word list and used random module

--- Hangman.py
+++ Hangman.py
@@ -1,40 +1,46 @@
-word = 'hangman'
+import random
+
+words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret ' \
+        'fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python' \
+        ' rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle' \
+        ' weasel whale wolf wombat zebra'.split()
+
+word = random.choice(words)
 
 word_list = list(word)
+# print(word_list)
 
-empty_list = ['-'] * len(word)
-display_string = str(empty_list)
+empty_list = ['-'] * len(word_list)
+# display_string = str(empty_list)
 
-print(display_string)
+print(word)
 
 
-attempts = 6
+attempts = 2
 while attempts > 0:
     if word_list == empty_list:
         break
-    user_word = input('Guess an alphabet: ')
+    user_word = input('Guess an alphabet: \n')
 
-    if user_word in word:
+    if user_word in word_list:
         i = 0
         while i < len(word_list):
             if word_list[i] == user_word:
                 empty_list[i] = user_word
             i+=1
-        print(empty_list)
+        print(empty_list,'\n')
+
+
 
     else:
         attempts -= 1
-        print('Alphabet not in the word, please try another, remaining attempts :', attempts)
+        if attempts == 0:
+            print('Sorry, no more attempts left. The word was', word.upper())
+        else:
+            print('Alphabet not in the word, please try another, remaining attempts :', attempts)
 
-print(empty_list)
 
-# while attempts > 0:
-#
-#     user_word = input('Guess an alphabet: ')
-#     if user_word in word:
-#             # empty_list[word.index(user_word)] = user_word
-#             empty_list.replace('-', user_word, len(word))
-#             print(empty_list)
-#     else:
-#         attempts -= 1
-#         print('Alphabet not in the word, please try another, remaining attempts :', attempts)
+
+
+# print(empty_list)
+
